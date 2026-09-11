03e8260b716584c413d68b4bfb04d2ea6ce1b5ed
vietnguyen-dev <vietnguyent22@gmail.com>
2026-09-10 18:51:35 -0700

    updated archive.tf

--- terraform/archive.tf
+++ terraform/archive.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "database_lambda_log_archive" {
-  bucket = "${local.name_prefix}-database-lambda-log-archive-${data.aws_caller_identity.current.account_id}"
+  bucket = "drawingboard-prod-archive"
   tags   = local.common_tags
 }
 
